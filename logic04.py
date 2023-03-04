bf8539b1f25ee2323d08e67a992bb93a586612e8
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-04 15:44:47 +0500

    log04

--- logic04.py
+++ logic04.py
@@ -7,7 +7,8 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    x = (int(a)%2 == 0), (int(b)%2 == 0)
+    x = ((int(a))%2 == 0), ((int(b))%2 == 0)
     return  x
 
 
+print(main(2, 1))
